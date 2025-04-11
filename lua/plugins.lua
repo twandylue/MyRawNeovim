691539891bb039ca34eaa44dce61fc9c1527876e
AndyLu <laserlue@gmail.com>
2025-04-11 11:59:47 -0600

    feat: update nvim-compliation config
    Update nvim-compliation config.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -501,8 +501,19 @@ local plugins = {
       "nvim-lua/plenary.nvim",
       -- if you want to enable coloring of ANSI escape codes in
       -- compilation output, add:
-      -- { "m00qek/baleia.nvim", tag = "v1.3.0" },
+      { "m00qek/baleia.nvim", tag = "v1.3.0" },
     },
+    config = function()
+      ---@type CompileModeOpts
+      vim.g.compile_mode = {
+        -- to add ANSI escape code support, add:
+        -- baleia_setup = true,
+
+        -- to make `:Compile` replace special characters (e.g. `%`) in
+        -- the command (and behave more like `:!`), add:
+        -- bang_expansion = true,
+      }
+    end,
   },
 }
 
